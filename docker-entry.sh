30fab697152862b0e441e9c8f20df3f5e41de0b0
Travis Glenn Hansen <travisghansen@yahoo.com>
2016-12-23 17:29:10 -0700

    support optional mysql usage

--- docker-entry.sh
+++ docker-entry.sh
@@ -2,13 +2,15 @@
 # Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
 # See License.txt for license information.
 
-echo "Starting MySQL"
-/entrypoint.sh mysqld &
+if [ "x${START_MYSQL,,}" != "xfalse" ]; then
+	echo "Starting MySQL"
+	/entrypoint.sh mysqld &
 
-until mysqladmin -hlocalhost -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" processlist &> /dev/null; do
-	echo "MySQL still not ready, sleeping"
-	sleep 5
-done
+	until mysqladmin -hlocalhost -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" processlist &> /dev/null; do
+		echo "MySQL still not ready, sleeping"
+		sleep 5
+	done
+fi
 
 echo "Starting platform"
 cd mattermost
