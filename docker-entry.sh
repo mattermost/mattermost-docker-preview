e4970c3481fd2e3be20a8d2a1fe4a0fb8e447826
James McClune <falcon78921@users.noreply.github.com>
2018-06-28 20:36:37 -0400

    docker test

--- docker-entry.sh
+++ docker-entry.sh
@@ -2,6 +2,9 @@
 # Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
 # See License.txt for license information.
 
+# Add Mattermost environment variables
+export PATH=${PATH}:/mm/mattermost/bin
+
 echo "Starting MySQL"
 /entrypoint.sh mysqld &
 
